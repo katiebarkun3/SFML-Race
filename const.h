b665fb9f4fe89de163adabfb6d4f315a8f1c173b
katiebarkun3 <92594853+katiebarkun3@users.noreply.github.com>
2022-02-13 19:31:16 +0300

    Add files via upload

---
+++ const.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+const float WINDOW_WIDTH = 600;
+const float WINDOW_HEIGHT = 950;
+const std::string WINDOW_TITLE = "ITSteps C++ & SFML Race";
+const float ROAD_SCALE_X = 0.375f;
+const float ROAD_SCALE_Y = 0.834f;
+const std::string ROAD_FILE_NAME = "road.jpg";
+const float ROAD_SPEED_X = 0.f;
+const float ROAD_SPEED_Y = 3.f;
+const std::string SPLASH_FILE_NAME = "race-splash.png";
+const std::string CAR_FILE_NAME = "car.png";
+const float CAR_SPEED = 5.f;
+const std::string BARRIER_FILE_NAME = "obs.png";
+const std::string GAME_OVER_FILE_NAME = "game_over.jpg";
